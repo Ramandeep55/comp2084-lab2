be6b4f2f964cb307a31e588d780b81ec82cb081e
Dell <Dell@DESKTOP-OKF6C2H>
2018-09-24 20:36:37 -0700

    complete lab2

---
+++ lab 2/Controllers/storeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab_2.Controllers
+{
+    public class storeController : Controller
+    {
+        // GET: store
+        public ActionResult Index()
+        {
+            return View();
+        }
+		// GET: Store/Product
+		public ActionResult product(String Products)
+		{
+			ViewBag.Products = Products;
+			return View();
+			}
+    }
+}
